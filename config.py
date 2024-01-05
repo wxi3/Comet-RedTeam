3557464097e8a60303712e08adc52dfc3ae86dec
wxi3 <70634411+wxi3@users.noreply.github.com>
2024-01-05 21:19:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-Fofa_email = 'safe@w3bsafe.cn'
-Fofa_key = '3c9d8994fe1340ab62f12da03f9eda67'
-VirusTotal_key = 'e09aa6d7202b50925347760b14fc1ae005797732e78d1bd4ae2c8fe06e6d31ec'
+Fofa_email = 'your_email'
+Fofa_key = 'your_fofa_key'
+VirusTotal_key = 'your_vt_key'
 headers=['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36','Mozilla/5.0 (Windows; U; Windows NT 5.1) Gecko/20070803 Firefox/1.5.0.12','Mozilla/5.0 (Macintosh; PPC Mac OS X; U; en) Opera 8.0','Mozilla/5.0 (iPhone; U; CPU like Mac OS X) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/4A93 Safari/419.3','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.12) Gecko/20080219 Firefox/2.0.0.12 Navigator/9.0.0.6','Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; 360SE)','Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0;Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; Maxthon/3.0)','Mozilla/5.0 (Windows NT 5.1) AppleWebKit/534.55.3 (KHTML, like Gecko) Version/5.1.5 Safari/534.55.3','Mozilla/5.0 (Linux; U; Android 4.0.3; zh-cn; M032 Build/IML74K) AppleWebKit/533.1 (KHTML, like Gecko)Version/4.0 MQQBrowser/4.1 Mobile Safari/533.1','Mozilla/5.0 (iPhone; CPU iPhone OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3']
 Web_port = [80, 88, 81, 443, 8080, 8443, 8000, 8001, 8008, 8081, 8888, 9000, 9080, 9090, 9443, 9999]
 
